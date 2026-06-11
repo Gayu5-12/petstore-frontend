7ac621655e9ba5cea95f1ae4a3f73902a8a8e655
Gayu5-12 <gayathridevi.2312@gmail.com>
2026-06-12 01:45:51 +0530

    Add files via upload

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_URL ||
-    "http://https://petstore-backend-2.onrender.com/api",
+  baseURL: "https://petstore-backend-2.onrender.com/api",
 });
 
 export default api;
